6cc1eff0f8cc3b3f1235d032befe61bdfa05b8f3
LUCASGOODPROGRAMMER <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2024-04-08 11:47:20 -0400

    Add files via upload

---
+++ CalculosFinanceiro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFinanceira
+{
+    public static class CalculosFinanceiro
+    {
+
+
+        public static double CalcularMontante(double taxa, double capital, double tempo)
+        {
+            taxa = taxa / 100;
+            return capital * (1 + taxa * tempo);
+        }
+        public static double CalcularCapital(double taxa, double montante, double tempo)
+        {
+            taxa = taxa / 100;
+            return montante / (1 + taxa * tempo);
+        }
+        public static double CalcularTaxa(double capital, double montante, double tempo)
+        {
+            return (montante - capital) / (capital * tempo);
+        }
+        public static double CalcularTempo(double montante, double capital, double taxa)
+        {
+            taxa = taxa / 100;
+            return (montante - capital) / (capital * taxa);
+        }
+        public static double CalcularJuros(double capital, double montante)
+        {
+            return montante - capital;
+        }         
+    }
+}
+
---
+++ ContatoCliente.cs
@@ -0,0 +1,74 @@
+﻿using BibliotecaFinanceira;
+
+Console.WriteLine("1-CALCULAR CAPITAL\n2-CALCULAR MONTANTE\n3-CALCULAR TAXA\n4-CALCULAR TEMPO");
+int escolha = Convert.ToInt32(Console.ReadLine());
+
+string mont = "Informe o montante: ";
+string tax = "Informe a taxa: ";
+string temp = "Informe o tempo: ";
+string capita = "Informe a capital: ";
+double valorJuros = 0;
+if (escolha == 1)
+{
+    Console.Write(mont);
+    double montante = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(tax);
+    double taxa = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(temp);
+    double tempo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Valor da capital = " + CalculosFinanceiro.CalcularCapital(taxa, montante, tempo));
+    valorJuros = CalculosFinanceiro.CalcularCapital(taxa, montante, tempo);
+    Console.WriteLine("Juros simples = " + (montante - valorJuros));
+}
+else if (escolha == 2)
+{
+    Console.Write(capita);
+    double capital = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(tax);
+    double taxa = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(temp);
+    double tempo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Valor do montante = " + CalculosFinanceiro.CalcularMontante(taxa,capital,tempo));
+    valorJuros = CalculosFinanceiro.CalcularMontante(taxa, capital, tempo);
+
+    Console.WriteLine("Juros simples = " + (valorJuros - capital));
+}
+else if (escolha == 3)
+{
+    Console.Write(mont);
+    double montante = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(capita);
+    double capital = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(temp);
+    double tempo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Valor da taxa = " + CalculosFinanceiro.CalcularTaxa(capital,montante,tempo));
+
+    Console.WriteLine("Juros simples = " + (montante - capital));
+}
+else if (escolha == 4)
+{
+    Console.Write(mont);
+    double montante = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(capita);
+    double capital = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(tax);
+    double taxa = Convert.ToDouble(Console.ReadLine());
+    
+    Console.WriteLine("Valor do tempo = " + CalculosFinanceiro.CalcularTempo(montante,capital,taxa));
+    Console.WriteLine("Juros simples = " + (montante - capital));
+}
+
+Console.ReadKey();
+
+
